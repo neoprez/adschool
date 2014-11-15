9676c03bbeb07cdc4245f8a4b64049953603679a
Rodny Perez <rodnyeperez@gmail.com>
2014-11-14 23:05:30 -0500

    added new values

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html >
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+	</head>
+	<body>
+		<div id="pageWrapper">
+			<header class="row">
+				<a class="large-4 logo columns">College Konnect</a>
+				<input type="text" name="search" placeholder="Search" class="large-3 columns" />
+				<a class="large-2 columns" name="login" href="login.php">Log in</a>
+				<a class="large-2 columns" name="signup">Sign Up</a>
+			</header>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('header.php'); ?>
+			<div class="row">
+				<div class="large-12 slider columns">
+					<img src="people.jpg"/>
+				</div>
+			</div>
+<?php include('footer.php'); ?>
