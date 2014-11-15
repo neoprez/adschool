cea1e03d71c85755b28a47fb2c31e18d4e3927ac
Rodny Perez <rodnyeperez@gmail.com>
2014-11-15 04:41:55 -0500

    added new things

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="styles.css" />
+		<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 	</head>
 	<body>
 		<div id="pageWrapper">
