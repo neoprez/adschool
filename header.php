68fac8d83eed1bf34b730db29c47982afa5097ca
Rodny Perez <rodnyeperez@gmail.com>
2014-11-14 23:28:39 -0500

    Adde style.css

--- header.php
+++ header.php
@@ -9,5 +9,5 @@
 				<a class="large-4 logo columns">College Konnect</a>
 				<input type="text" name="search" placeholder="Search" class="large-3 columns" />
 				<a class="large-2 columns" name="login" href="login.php">Log in</a>
-				<a class="large-2 columns" name="signup">Sign Up</a>
+				<a class="large-2 columns" name="signup" href="signup.php">Sign Up</a>
 			</header>
