f0e4f963eeb316e034070ea33556bc77b602aee7
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2014-11-14 22:28:45 -0500

    This paulo

---
+++ login.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Sign Up</title>
+	</head>
+	<body>
+		<!--Sign up using FB account-->
+		<div id="facebook_signup">
+			
+		</div>
+		<div id="signup_div">
+			<form id="signup_form" action="signup.php">
+				<label>First Name</label>
+				<input type="text" id="form_firstname" />
+
+				<label>Last Name</label>
+				<input type="text" id="form_lastname" />
+			</form>
+		</div>
+	</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,67 @@
+<html>
+	<head>
+		<title>Sign Up</title>
+	</head>
+	<body>
+		<!--Sign up using FB account-->
+		<div id="facebook_signup">
+			
+		</div>
+		<div id="signup_div">
+			<form id="signup_form" action="signup.php">
+				
+				<label>First Name</label>
+				<input type="text" id="form_firstname" />
+
+				<br />
+
+				<label>Last Name</label>
+				<input type="text" id="form_lastname" />
+
+				<br />
+
+				<label>User type</label>
+				<select>
+					<option value="1" selected="selected">Faculty Member</option>
+					<option value="2">Student</option>
+					<option value="3">Organization Member</option>
+				</select>
+
+				<br />
+
+				<label>School</label>
+				<select id="form_school">
+					<option value="" selected="selected">None</option>
+					<option value="1">Lehman College</option>
+					<option value="2">City College of New York</option>
+					<option value="3">Columbia University</option>
+					<option value="0">Other</option>
+				</select>
+
+				<br />
+
+				<label>Email</label>
+				<input type="text" id="form_email" />
+
+				<br />
+
+				<label>Re-type your Email</label>
+				<input type="text" id="form_email_retype" />
+
+				<br />
+
+				<label>Password</label>
+				<input type="password" id="form_password">
+
+				<br />
+
+				<label>Re-type Password</label>
+				<input type="password" id="form_password_retype" />
+
+				<br />
+
+				
+			</form>
+		</div>
+	</body>
+</html>
