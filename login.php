edb7c14b49fc6812bf47e1bcf2740200c5ec4883
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2014-11-15 01:45:04 -0500

    Update login/forgot username

--- login.php
+++ login.php
@@ -9,57 +9,51 @@
 
 	// $stmt = mysqli_stmt_init($mysqli);
 
-	// echo $username;
-	// echo $password;
-
 	// if($username != '' || $password != '') {
 
 	// 	if (mysqli_stmt_prepare($stmt, "SELECT * FROM user WHERE username='". $username ."'"." AND password='". $password ."'")) {
 	// 		mysqli_stmt_execute($stmt);
  //    		mysqli_stmt_bind_result($stmt, $result);
  //    		mysqli_stmt_fetch($stmt);
-
- //    		echo $result;
 	// 	} else {
 
 	// 	}
 	// }
 
 ?>
-		<!--Sign up using FB account-->
-		<div class="row center">
-			<div class="large-12 columns">
-				<div class="panel">
-					<div id="signup_div">
-						<form id="signup_form" action="timeline.php" method="POST">
-							<!-- <label>First Name</label> -->
-							
-							<div class="large-12 columns">
-								<input type="text" id="username" placeholder="Username" style="padding: 5px;" />
-							</div>
+	<div class="row center">
+		<div class="large-12 columns">
+			<div class="panel">
+				<div id="signup_div">
+					<form id="signup_form" action="login.php" method="POST">
+						<!-- <label>First Name</label> -->
+						
+						<div class="large-12 columns">
+							<input type="text" name="username" id="username" placeholder="Username" style="padding: 5px;" />
+						</div>
 
-							<!-- <label>Last Name</label> -->
-							<div class="large-12 columns">
-								<input type="password" id="password" placeholder="Password" style="padding: 5px;" />
-							</div>
+						<!-- <label>Last Name</label> -->
+						<div class="large-12 columns">
+							<input type="password" name="password" id="password" placeholder="Password" style="padding: 5px;" />
+						</div>
 
-							<div class="large-12 columns">
-								<input class="small radius button" type="submit" value="Login" />
-							</div>
+						<div class="large-12 columns">
+							<input class="small radius button" type="submit" value="Login" />
+						</div>
 
-							<div class="large-6 medium-6 columns">
-								<a href="forgotusername.php">Forgot Username?</a>
-							</div>
+						<div class="large-6 medium-6 columns">
+							<a href="forgotusername.php">Forgot Username?</a>
+						</div>
 
-							<div class="large-6 medium-6 columns">
-								<a href="forgotpassword.php">Forgot Password?</a>
-							</div>
+						<div class="large-6 medium-6 columns">
+							<a href="forgotpassword.php">Forgot Password?</a>
+						</div>
 
-						</form>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
+	</div>
 <?php
 	include ('footer.php');
 ?>
