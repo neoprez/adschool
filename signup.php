6ff7b52410f191afebeedb725c8247ae428da33b
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2014-11-14 21:48:04 -0500

    Creating signup form

--- signup.php
+++ signup.php
@@ -9,11 +9,35 @@
 		</div>
 		<div id="signup_div">
 			<form id="signup_form" action="signup.php">
+				
 				<label>First Name</label>
 				<input type="text" id="form_firstname" />
 
+				<br />
+
 				<label>Last Name</label>
 				<input type="text" id="form_lastname" />
+
+				<br />
+
+				<label>Email</label>
+				<input type="text" id="form_email" />
+
+				<br />
+
+				<label>Re-type your Email</label>
+				<input type="text" id="form_retype_email" />
+
+				<br />
+
+				<label>School</label>
+				<select id="form_school">
+					<option value="" selected="selected">None</option>
+					<option value="1">Lehman College</option>
+					<option value="2">City College of New York</option>
+					<option value="3">Columbia University</option>
+					<option value="0">Other</option>
+				</select>
 			</form>
 		</div>
 	</body>
