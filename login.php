ae0472ce2bdb11646567068619d1bf4f26c38f39
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2014-11-15 00:50:48 -0500

    Create connect config

--- login.php
+++ login.php
@@ -1,19 +1,43 @@
 <?php
 	include ('header.php');
+	include_once ('connect.php');
+
 ?>
 		<!--Sign up using FB account-->
 		<div id="facebook_signup">
 			
 		</div>
 		<div class="row center">
-			<div id="signup_div"`class="row">
-				<form id="signup_form" action="signup.php">
-					<!-- <label>First Name</label> -->
-					<input type="text" id="username" placeholder="Username" />
+			<div class="large-12 columns">
+				<div class="panel">
+					<div id="signup_div">
+						<form id="signup_form" action="login.php" method="POST">
+							<!-- <label>First Name</label> -->
+							
+							<div class="large-12 center" style="margin: 5px;">
+								<input type="text" id="username" placeholder="Username" style="padding: 5px;" />
+							</div>
 
-					<!-- <label>Last Name</label> -->
-					<input type="password" id="password" placeholder="Password" />
-				</form>
+							<!-- <label>Last Name</label> -->
+							<div class="large-12 center" style="margin: 5px;">
+								<input type="password" id="password" placeholder="Password" style="padding: 5px;" />
+							</div>
+
+							<div class="large-12 columns center" style="margin: 5px;">
+								<input class="small radius button" type="submit" value="Login" />
+							</div>
+
+							<div class="large-6 medium-6 columns">
+								<a href="forgotusername.php">Forgot Username?</a>
+							</div>
+
+							<div class="large-6 medium-6 columns">
+							<a href="forgotpassword.php">Forgot Password?</a>
+							</div>
+
+						</form>
+					</div>
+				</div>
 			</div>
 		</div>
 <?php
