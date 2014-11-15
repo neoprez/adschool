f6a6db621821e196dae6adcb76f51ca1638a8e1a
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2014-11-15 04:33:40 -0500

    connect fix

--- connect.php
+++ connect.php
@@ -3,8 +3,6 @@
 	$myuser = "root";
 	$mypassw = "root";
 	$mybd = "COLLEGE_KONNET";
-	$mypassw = "";
-	$mybd = "college_konnet";
 
 	$mysqli = mysqli_connect($myhost, $myuser, $mypassw, $mybd) or die("Error " . mysqli_error($mysqli)); 
 ?>
